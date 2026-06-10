7ab6bfbb69f2b0934d72dcf574220ab9bf4076d6
ALi6108 <ali_abidin_33rpl@student.smktelkom-mlg.sch.id>
2026-06-10 21:30:02 +0700

    yy

--- services/members.api.ts
+++ services/members.api.ts
@@ -44,6 +44,10 @@ export const membersApi = {
   uploadPhoto: (id: string, file: File) => {
     const formData = new FormData();
     formData.append('photo', file);
-    return api.post(`/api/v1/members/${id}/photo`, formData);
+    return api.post(`/api/v1/members/${id}/photo`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
   },
 };
